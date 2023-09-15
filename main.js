4d561a1b69c7935b94f5c00e169ee0169438e3ec
lesavantdon <144865003+lesavantdon@users.noreply.github.com>
2023-09-14 17:06:18 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+//my age 
+var myAge = 29;
+//early years
+var earlyYears= 2;
+earlyYears *= 10.5; 
+console.log(earlyYears);
+ var earlyYears = 21;
+ myAge-=2
+ console.log(myAge);
+ //later years is 29 - 2
+var laterYears = myAge;
+laterYears *= 4;
+console.log(laterYears);
+var myAgeInDogYears = laterYears + earlyYears;
+console.log(myAgeInDogYears);
+//return string to lowercase 
+var myName = 'aneet.';
+console.log(myName.toLowerCase());
+//string inerpolation that displays my age and my age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
